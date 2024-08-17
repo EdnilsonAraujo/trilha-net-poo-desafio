d044560e8acfc8f33d30cf5953985ea4157a5d15
Ednilson Araújo <33930720+EdnilsonAraujo@users.noreply.github.com>
2024-08-17 08:07:33 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Watsapp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Nokia", imei: "11111111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
